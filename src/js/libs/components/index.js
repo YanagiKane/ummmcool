87371162730d507458de062abc22cf2efce6a342
YanagiKane <86703212+YanagiKane@users.noreply.github.com>
2023-06-14 01:38:21 +0700

    em

---
+++ src/js/libs/components/index.js
@@ -0,0 +1,7 @@
+import { initTheme } from "./theme/theme";
+import { initNavbar } from "./navbar/navbar";
+import { initBackToTop } from "./backtotop/backtotop";
+
+window.initTheme = initTheme;
+window.initNavbar = initNavbar;
+window.initBackToTop = initBackToTop;
